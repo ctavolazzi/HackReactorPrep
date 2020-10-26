cb28c666b7567a16222960c8ddd7532f2a260e81
Christopher Tavolazzi <ctavolazzi@gmail.com>
2020-10-26 21:07:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let displayMessage = "Good afternoon!"
+
+function greet (str) {
+  return str;
+}
+
+// code to be displayed goes here:
+displayMessage = greet("Hola!")
+
+document.getElementById("display").innerHTML = displayMessage
+
+console.log("hello, world")
